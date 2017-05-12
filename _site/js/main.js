e84524763108890421d196f39deca739683ec98d
kyomi <kyomiwade@Kyomis-MacBook-Pro.local>
2017-05-12 11:59:31 +0800

    poem page

--- _site/js/main.js
+++ _site/js/main.js
@@ -3,7 +3,8 @@ $(document).ready(function () {
   $('.poem-layout').masonry({
     // options
     itemSelector: '.poem-item',
-    columnWidth: 200
+    columnWidth: 200,
+    gutter: 10
   });
 
 });
